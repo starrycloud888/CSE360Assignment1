cd2a130b6005381b3ff6821b074020e2b9a6be80
paulc <paulc@DESKTOP-9IM4AUU>
2020-10-02 22:24:47 -0700

    First commit of assignment 1.

---
+++ CSE360Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,34 @@
+/*
+ * Name: Paul Chang
+ * Description: uhhh
+ */
+
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+	  total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+	  return 0;
+  } // returns total
+  
+  public void add (int value) {
+	  
+  } // adds
+
+  public void subtract (int value) {
+	  
+  } // subtracts
+
+  public String toString () {
+	  return "";
+  } // returns string
+
+  public void clear() {
+	  
+  } // deletes something?
+}
